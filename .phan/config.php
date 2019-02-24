4632edd06b767d33e62af24228c4d40e5fce5ff9
zoranzoki21 <zorandori4444@gmail.com>
2019-02-24 07:35:32 +0100

    Migrate to newer phan
    Bug: T216903
    Change-Id: I1d2a977f8c8d30ea376e36a689bdeeef795b9439

---
+++ .phan/config.php
@@ -0,0 +1,4 @@
+<?php
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg['suppress_issue_types'][] = 'PhanParamSignatureMismatch';
+return $cfg;
--- tests/phan/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-$cfg['suppress_issue_types'][] = 'PhanParamSignatureMismatch';
-return $cfg;
